72f52bbe491ae1fd5683def17bbbf4a326dc54f3
LouieAlcantara <113391125+LouieAlcantara@users.noreply.github.com>
2022-10-10 19:28:58 -0300

    version1.1

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
 print('rodando o sistema de gerenciamento de jogos')
+dic{
+    nome: input('Ola digite seu nome')
+    idade: input('digite sua idade')
+}
+print('ola {nome} parabens por seus {idade} anos')
