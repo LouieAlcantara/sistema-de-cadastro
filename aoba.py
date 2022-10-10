ba49dc0b2c920128d56b4512cb66936c748ad2ce
LouieAlcantara <luna.alcantara19@gmail.com>
2022-10-10 20:55:46 -0300

    primeiro teste

---
+++ aoba.py
@@ -0,0 +1,2 @@
+def punt ():
+    print('-'*8)
--- app.py
+++ app.py
@@ -1,2 +1,1 @@
-console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("aplicação esta se conectando ao banco de dados")
+print('seja bem vindo para essa aventura')
